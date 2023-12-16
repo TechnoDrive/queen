63c418f35d7f8688f93e1472ef3ccb9786137cfb
TechnoDrive <hextanium.on.discord@gmail.com>
2023-12-16 08:55:51 -0500

    add: operation methods to `Expression`

--- src/main/kotlin/expressions/Expression.kt
+++ src/main/kotlin/expressions/Expression.kt
@@ -2,4 +2,7 @@ package expressions
 
 interface Expression {
     fun value(): Expression
+
+    operator fun plus(other: Expression): Expression
+    operator fun times(other: Expression): Expression
 }
