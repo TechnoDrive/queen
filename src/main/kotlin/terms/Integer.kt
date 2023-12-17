d69f7a4f5c833adbb614921c8d08bc9749af5b21
TechnoDrive <hextanium.on.discord@gmail.com>
2023-12-17 18:19:36 -0500

    refactor: remove the `Integer` class

--- src/main/kotlin/terms/Integer.kt
+++
@@ -1,8 +0,0 @@
-package terms
-
-import expressions.Expression
-
-// TODO: replace with a `Rational` class with a denominator of 1
-class Integer(val number: Int) : Expression {
-    override fun value() = number
-}
