3155de4dde276291a13b89cc99dc2fbd001e662e
TechnoDrive <hextanium.on.discord@gmail.com>
2023-12-18 22:12:32 -0500

    add: `simplify` method on `Expression`

--- src/main/kotlin/expressions/Expression.kt
+++ src/main/kotlin/expressions/Expression.kt
@@ -3,6 +3,8 @@ package expressions
 interface Expression {
     fun value(): Any?
 
+    fun simplify(): Expression
+
     operator fun plus(other: Expression) = Sum(this, other)
     operator fun times(other: Expression) = Product(this, other)
 }
