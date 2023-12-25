163d5e07463ad38959760379a8cbbe6cf395261e
TechnoDrive <hextanium.on.discord@gmail.com>
2023-12-25 00:06:55 -0500

    refactor: implement the required `simplify` method and add a `toString` method; todo: ponder `value` vs `simplify`

--- src/main/kotlin/terms/Variable.kt
+++ src/main/kotlin/terms/Variable.kt
@@ -1,7 +1,9 @@
 package terms
 
 import expressions.Expression
-
 class Variable(val label: String) : Expression {
     override fun value() = label
+    override fun simplify() = this
+
+    override fun toString() = label
 }
