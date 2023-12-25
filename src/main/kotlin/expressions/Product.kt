5b2056d05d0d9e2f1817ef588f0fb61a94bb94c9
TechnoDrive <hextanium.on.discord@gmail.com>
2023-12-25 00:05:44 -0500

    refactor: completely remodel `Product`, move away from constructor parameters so we can simplify instantly, add a primitive `simplify` method, add a `factors` method to be used for future simplifications, add a basic `toString` method because we're not in a data class

--- src/main/kotlin/expressions/Product.kt
+++ src/main/kotlin/expressions/Product.kt
@@ -1,7 +1,34 @@
 package expressions
 
 import operations.BinaryOperation
+import terms.Rational
 
-class Product(override val lhs: Expression, override val rhs: Expression) : BinaryOperation {
+class Product(lhs: Expression, rhs: Expression) : BinaryOperation {
+    override val lhs = lhs.simplify()
+    override val rhs = rhs.simplify()
+
     override fun value() = lhs * rhs
+
+    fun factors(): Array<Expression> = when {
+        lhs is Product && rhs is Product -> arrayOf(*lhs.factors(), *rhs.factors())
+        lhs is Product && rhs is Rational -> arrayOf(*lhs.factors(), rhs)
+        lhs is Rational && rhs is Product -> arrayOf(lhs, *rhs.factors())
+
+        else -> arrayOf(lhs, rhs)
+    }
+
+    // this method is currently NOT commutative
+    // i've written the factors method above to change that
+    // the goal is, when simplifying, to get all the factors
+    // and then compose all rationals, no matter how deep
+    // this logic can then be extended to like-terms easily
+    //
+    // TODO: use factors to do the above
+    override fun simplify(): Expression = when {
+        lhs is Rational && rhs is Rational -> (lhs * rhs).simplify()
+        lhs is Product && rhs is Product -> Product(lhs.lhs * rhs.lhs, lhs.rhs * rhs.rhs)
+        else -> this
+    }
+
+    override fun toString() = "Product($lhs, $rhs)"
 }
